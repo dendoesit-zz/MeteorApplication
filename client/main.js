a9973646ce4e20960483e0947ba9f12474c4beb9
dendoesit <dan_claw20@yahoo.com>
2016-05-23 23:19:11 +0200

    full commit

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
+
+import './main.html';
+import '../imports/ui/body.js';
+
+
